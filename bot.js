6b23efda2bf5fb9cf17d740afad5fc47c0dbc1ba
Mahmoud Gaber <62293072+DEV1L-Projects@users.noreply.github.com>
2020-05-22 00:52:19 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,28 @@
+const discord = require('discord.js');
+const client = new discord.Client();
+/* 
+GiveAway's Auto Reaction
+Please Don't Change Anything
 
+By : ! - S O L A R ツ#0096
+*/
+
+// ======================================================= //
+
+// GiveAwayBot
+client.on('message', message => {
+    if (message.author.id !== '294882584201003009') return;
+    message.react('🤔');
+});
+
+// ======================================================= //
+
+// Santa Wumpus
+client.on('message', message => {
+    if (message.author.id !== '582537632991543307') return;
+    message.react('🤔');
+});
+
+// ======================================================= //
+
+client.login(process.env.BOT_TOKEN);
